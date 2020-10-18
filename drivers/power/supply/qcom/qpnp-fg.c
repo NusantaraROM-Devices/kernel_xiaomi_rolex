Jebaitedneko/android_kernel_10or_E-4.9

DRIVERS: POWER: SUPPLY: QCOM: qpnp-fg: Fix 2% battery drop

drivers/power/supply/qcom/qpnp-fg.c
@@ -8035,7 +8035,11 @@ static int fg_common_hw_init(struct fg_chip *chip)
 	}
 
 	rc = fg_mem_masked_write(chip, settings[FG_MEM_DELTA_SOC].address, 0xFF,
-			soc_to_setpoint(settings[FG_MEM_DELTA_SOC].value),
+			#ifdef CONFIG_MACH_TENOR_E
+			1,
+			#else
+			settings[FG_MEM_DELTA_SOC].value == 1 ? 1 : soc_to_setpoint(settings[FG_MEM_DELTA_SOC].value),
+			#endif
 			settings[FG_MEM_DELTA_SOC].offset);
 	if (rc) {
 		pr_err("failed to write delta soc rc=%d\n", rc);

NusantaraROM-Devices/kernel_xiaomi_rolex

drivers/power/supply/qcom/qpnp-fg.c
@@ -8033,7 +8033,11 @@ static int fg_common_hw_init(struct fg_chip *chip)
 	}
 
 	rc = fg_mem_masked_write(chip, settings[FG_MEM_DELTA_SOC].address, 0xFF,
-			soc_to_setpoint(settings[FG_MEM_DELTA_SOC].value),
+			#ifdef CONFIG_MACH_XIAOMI_ROLEX
+			1,
+			#else
+			settings[FG_MEM_DELTA_SOC].value == 1 ? 1 : soc_to_setpoint(settings[FG_MEM_DELTA_SOC].value),
+			#endif
 			settings[FG_MEM_DELTA_SOC].offset);
 	if (rc) {
 		pr_err("failed to write delta soc rc=%d\n", rc);